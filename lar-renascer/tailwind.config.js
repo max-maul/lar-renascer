88c65695ef97ce81a993a3342db2b411355b85c9
Max Maul <maxbuzzarello@gmail.com>
2024-05-02 20:02:14 -0300

    cores custom

--- lar-renascer/tailwind.config.js
+++ lar-renascer/tailwind.config.js
@@ -7,7 +7,9 @@ export default {
   theme: {
     extend: {
       colors: {
-        "custom-pink": "#FFA4A4"
+        "custom-pink": "#FFA4A4",
+        "custom-dark-pink": "#BA1971",
+        "custom-dark-yellow": "#B7932D"
       }
     },
   },
